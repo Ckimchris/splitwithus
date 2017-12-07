1a2c40e20a8af5424757add72ddbd052e356a6cb
ChrisKim <ckimchris@Chris-Kims-MacBook-Pro-2.local>
2017-12-06 17:31:31 -0800

    commiting new changes

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import NavBar from './NavBar/NavBar';
-import ServicesContainer from './containers/ServicesContainer';
-
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch
-} from 'react-router-dom';
+import App from './App.js';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -17,11 +9,7 @@ import store from './store';
 render(
   <Provider store={store}>
     <MuiThemeProvider>
-      <div>
-        <NavBar />
-        <ServicesContainer type={'Video'} services={['Netflix', 'Hulu']} />
-        <ServicesContainer type={'Music'} services={['Spotify', 'Pandora']} />
-      </div>
+      <App />
     </MuiThemeProvider>
   </Provider>, document.getElementById('content')
 );
--- server/server.js
+++ server/server.js
@@ -4,40 +4,91 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+
+// middleware
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const GithubStrategy = require('passport-github').Strategy;
+const session = require('express-session');
 
 const port = 8080;
-
-/**
- * static files middleware
- */
+/***************************
+ * Static files middleware
+ ***************************/
 app.use(function (req, res, next) {
   if (req.url.match(/.js$|.html$|.css$|.png$|.woff|.woff2|.tff$/)) {
     res.sendFile(path.join(__dirname + '/..' + req.url));
-  }  else next();
+  } else next();
 });
 
-/**
- * body and cookie parsing middleware
- */
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
+/***************************
+ * Body and cookie parsing middleware
+ ***************************/
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 
-/**
- * ROUTING
+/***************************
+ * Initialize Express Sessions
+ ***************************/
+app.use(session({
+  key: 'user_sid',
+  secret: 'nyancatmeow',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000
+  }
+}));
+/*
+ * Middleware will check if user's cookie is still saved in browser and if user is not set, then automatically log the user out.
  */
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie('user_sid');
+  }
+  next();
+});
+
+/*
+ * Middleware to check if user is in an active session
+ */
+const sessionChecker = (req, res, next) => {
+  if (req.session.user && req.cookies.user_sid) {
+    res.redirect('/servicespage');  // figure out which route to redirect to
+  } else {
+    next();
+  }
+};
+
+/***************************
+ OAuth
+ ***************************/
+passport.use(new GithubStrategy({
+  clientID: '95a121558b40b1023f42',
+  clientSecret: 'ca15e822b7c2211955486ea211a0567885f71a30',
+  callbackURL: 'http://localhost:8080/auth/github/callback'
+},
+function (accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}
+));
+/***************************
+ * ROUTING
+ ***************************/
 
 // Static HTML routing
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + './../index.html'));
 });
 
-/**
+/***************************
  * Database Setup
- */
+ ***************************/
 const db = require('./db/dbConfig');
 
 // Database Controllers
@@ -45,9 +96,9 @@ const userController = require('./controllers/userController');
 const groupController = require('./controllers/groupController');
 const serviceController = require('./controllers/serviceController');
 
-/**
+/***************************
  *Database Connection
- */
+ ***************************/
 // db.connect()
 //   .then(obj => {
 //     console.log('connected to db');
@@ -58,16 +109,18 @@ const serviceController = require('./controllers/serviceController');
 //   })
 //   .catch(error => console.log('error connecting'));
 
-// Server Routes
-
+/***************************
+Server Routes
+ ***************************/
 // All Users
 app.get('/users', userController.allUsers);
 
 // All Services
 app.get('/services', serviceController.allServices);
 
-// Login Route
+// Login/Logout Routes
 app.get('/login', userController.login);
+app.get('/logout', userController.logout);
 
 // Forgot Password
 app.get('/forgotpw', userController.forgotpw);
@@ -88,6 +141,14 @@ app.post('/leavegroup', (req, res) => {
 app.get('/home', (req, res) => {
   console.log('hello home');
   res.send('hello home');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + './../index.html'));
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + './../index.html'));
 });
 
 app.listen(port, () => {
